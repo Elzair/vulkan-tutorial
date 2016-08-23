b659bbed67c73e3ec4c87ab1eb80c8876296666f
Philip Woods <elzairthesorcerer@gmail.com>
2016-08-22 20:12:09 -0400

    Added lesson on logical device creation

---
+++ lessons/06-logical/queue.hpp
@@ -0,0 +1,44 @@
+#ifndef __QUEUE_HPP__
+#define __QUEUE_HPP__
+
+#include "common-includes.hpp"
+
+struct QueueFamilyIndices
+{
+  int graphics_family = -1;
+
+  bool isComplete(  )
+  {
+    return this->graphics_family >= 0;
+  }
+};
+
+QueueFamilyIndices findQueueFamilies( VkPhysicalDevice device )
+{
+  QueueFamilyIndices indices;
+
+  uint32_t qf_count = 0;
+  vkGetPhysicalDeviceQueueFamilyProperties( device, &qf_count, nullptr );
+  std::vector<VkQueueFamilyProperties> qfamilies( qf_count );
+  vkGetPhysicalDeviceQueueFamilyProperties( device, &qf_count, qfamilies.data() );
+
+  int i = 0;
+  for ( const auto& qfamily : qfamilies )
+  {
+    if ( qfamily.queueCount > 0 && qfamily.queueFlags & VK_QUEUE_GRAPHICS_BIT )
+    {
+      indices.graphics_family = i;
+    }
+
+    if ( indices.isComplete() )
+    {
+      break;
+    }
+
+    i++;
+  }
+
+  return indices;
+}
+
+#endif
