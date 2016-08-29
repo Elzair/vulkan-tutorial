5ede063bf4ef65500c94478f7c10f6d9d70dbca1
Philip Woods <elzairthesorcerer@gmail.com>
2016-08-29 13:11:52 -0400

    Added lesson on creating uniform descriptors

---
+++ lessons/23-descriptorlayout/base-includes.hpp
@@ -0,0 +1,16 @@
+#ifndef __BASE_INCLUDES_HPP__
+#define __BASE_INCLUDES_HPP__
+
+#include <array>
+#include <vector>
+#include <set>
+#include <chrono>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#endif
